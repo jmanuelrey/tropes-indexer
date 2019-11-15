7f6f5f00606c7e8cadad5ebcb7fca002682cf874
DiegoMartinezCela <diego.martinez.cela@udc.es>
2019-11-15 15:46:54 +0100

    Añadidos laconic y tropos relacionados
    Añadidos string de laconic y lista de strings de tropos relacionados a los tropos.

--- src/main/java/muei/riws/tropes/indexer/TropeIndexer.java
+++ src/main/java/muei/riws/tropes/indexer/TropeIndexer.java
@@ -34,20 +34,72 @@ import org.json.simple.parser.ParseException;
 public class TropeIndexer {
     
 	private static final String INDEX_NAME = "tropes";
+	private static final String NODE_NAME = "kiiKFUc";
+	
+	public static class Pair<T, U> {         
+	    public final T trope;
+	    public final U laconic;
+
+	    public Pair(T t, U u) {         
+	        this.trope= t;
+	        this.laconic= u;
+	     }
+	 }
 	
 	private static class Trope {
 		public String Name;
 		public String Content;
 		public String Url;
+		public String Laconic;
+		public List<String> RelatedTropos;
 		public Map<String, List<String>> Media = new HashMap<String, List<String>>();
 		
-		public Trope(String name, String content, String url, Map<String, List<String>> media) {
+		public Trope(String name, String content, String url, String laconic, List<String> relatedTropos, Map<String, List<String>> media) {
 			Name = name;
 			Content = content;
 			Url = url;
+			Laconic = laconic;
+			RelatedTropos = relatedTropos;	
 			Media = media;
 		}
-		// TODO laconic, related tropes
+	}
+	
+	private static XContentBuilder createTropeSettings() throws IOException {
+		XContentBuilder builder = XContentFactory.jsonBuilder();
+		builder.startObject()
+			.startObject("settings")
+				.startObject("analysis")
+					.startObject("filter")
+						.startObject("english_stop")
+							.field("type", "stop")
+							.field("stopwords", "_english_")
+						.endObject()
+						.startObject("english_stemmer")
+							.field("type", "stemmer")
+							.field("language", "english")
+						.endObject()
+						.startObject("english_possessive_stemmer")
+							.field("type", "stemmer")
+							.field("language", "possessive_english")
+						.endObject()
+					.endObject()
+					.startObject("analyzer")
+						.startObject("rebuilt_english")
+							.field("tokenizer", "standard")
+							.startArray("filter")
+								.value("english_possessive_stemmer")
+								.value("lowercase")
+								.value("english_stop")
+								.value("english_stemmer")
+							.endArray()
+							.field("stopwords", "_english_")
+						.endObject()
+					.endObject()
+				.endObject()
+			.endObject()
+		.endObject();
+		
+		return builder;
 	}
 	
 	private static XContentBuilder createTropeMapping() throws IOException {
@@ -56,15 +108,34 @@ public class TropeIndexer {
         builder.startObject()
         	.startObject("_doc")
         		.startObject("properties")
-        			.startObject("name").field("type", "text").endObject()
-                    .startObject("content").field("type", "text").endObject()
-                    .startObject("laconic").field("type", "text").endObject()
-            		.startObject("url").field("type", "text").endObject()
-            		.startObject("related_tropes").field("type", "text").endObject()
+        			.startObject("name")
+        				.field("type", "text")
+        				.startObject("fields")
+        					.startObject("raw")
+        						.field("type", "keyword")
+        					.endObject()
+    					.endObject()
+    				.endObject()
+                    .startObject("content")
+                    	.field("type", "text")
+                	.endObject()
+                    .startObject("laconic")
+                    	.field("type", "text")
+                	.endObject()
+            		.startObject("url")
+            			.field("type", "text")
+        			.endObject()
+            		.startObject("related_tropes")
+            			.field("type", "text")
+        			.endObject()
             		.startObject("media")
             			.startObject("properties")
-	                    	.startObject("media_type").field("type", "text").endObject()
-	                    	.startObject("media_urls").field("type", "text").endObject()
+	                    	.startObject("media_type")
+	                    		.field("type", "text")
+                    		.endObject()
+	                    	.startObject("media_urls")
+	                    		.field("type", "text")
+                    		.endObject()
     	                .endObject()	                
 	                .endObject()
             	.endObject()
@@ -77,7 +148,7 @@ public class TropeIndexer {
     private static TransportClient createClient() throws IOException {
         // ES Client settings
         Settings settings = Settings.builder()
-                .put("node.name", "kiiKFUc").build();
+                .put("node.name", NODE_NAME).build();
         
         // Create a transport client
         // Port 9300 for node-to-node communication
@@ -93,7 +164,6 @@ public class TropeIndexer {
         CreateIndexRequest request = new CreateIndexRequest(INDEX_NAME);
         // TODO comprobar si el indice existe y eliminarlo
         XContentBuilder builder = createTropeMapping();
-        
         request.mapping("_doc", builder);
         
         TransportClient client = createClient();
@@ -109,6 +179,8 @@ public class TropeIndexer {
 	    	.field("name", trope.Name)
 	    	.field("content", trope.Content)
 	    	.field("url", trope.Url)
+	    	.field("laconic", trope.Laconic)
+	    	.field("related_tropes", trope.RelatedTropos)
     		.startArray("media");
 		    		for(String mediaType : trope.Media.keySet()) {
 		    			contentBuilder
@@ -132,6 +204,26 @@ public class TropeIndexer {
         client.close();
     }
     
+    private static List<String> parseJsonRelatedTropes(JSONArray nonMediaLinks) {
+    	
+    	List<String> relatedTropes = new ArrayList<String>();
+    	
+    	Pattern relatedPattern = Pattern.compile("php/Main/*");
+    	
+    	for(Object urlObj : nonMediaLinks) {
+    		String url = (String) urlObj;
+    		// Extract the related tropes from the url (format "...php/Main/[Trope])
+    		// TODO comprobar que la url es realmente un tropo
+    		// NOTA: aunque no lo fuese no afectaría especialmente a las estadísticas, ya que todos los tropos tendrían más o menos los mismos
+    		// enlaces "erróneos".
+    		Matcher m = relatedPattern.matcher(url);
+    		if(m.find())
+    			relatedTropes.add(url);
+    	}
+    	
+    	return relatedTropes;
+    	
+    }
     
     private static Map<String, List<String>> parseJsonMedia(JSONArray mediaLinks) {
     	//String mediaType = (String) mediaContent.get(key)
@@ -141,81 +233,106 @@ public class TropeIndexer {
     	
     	for(Object urlObj : mediaLinks) {
     		String url = (String) urlObj;
-    		// Extract the media type from the url (format "...php/[Media type]/...(
+    		// Extract the media type from the url (format "...php/[Media type]/...)
     		// TODO enum de medios? (para alguna funcionalidad)
     		Matcher m = mediaPattern.matcher(url);
-    		m.find();
-    		String mediaType = m.group(1);
-    		if(mediaList.containsKey(mediaType)) {
-    			mediaList.get(mediaType).add(url);
-    		} else {
-    			List<String> urls = new ArrayList<String>();
-    			urls.add(url);
-    			mediaList.put(mediaType, urls);
+    		if(m.find()) {
+	    		String mediaType = m.group(1);
+	    		if(mediaList.containsKey(mediaType)) {
+	    			mediaList.get(mediaType).add(url);
+	    		} else {
+	    			List<String> urls = new ArrayList<String>();
+	    			urls.add(url);
+	    			mediaList.put(mediaType, urls);
+	    		}
     		}
     	}
     	
     	return mediaList;
     }
     
+    private static Trope parseJsonLaconic(JSONObject jsonContent) {
+		// Get trope name
+		String tropeName = (String) jsonContent.get("title");
+		
+		// Get laconic content
+		String laconic = (String) jsonContent.get("content");
+
+    	Trope trope = new Trope(tropeName, "", "", laconic, new ArrayList<String>(), new HashMap<String, List<String>>());
+    	
+    	return trope;
+    }
+    
 	@SuppressWarnings("unchecked")
 	private static Trope parseJsonTrope(JSONObject jsonContent) {
 
-		//Get trope name
-		String tropeName = (String) jsonContent.get("title");
+		// TODO comprobar existencia de elementos
 		
-		//Get trope content
+		// Get trope name
+		String tropeName = (String) jsonContent.get("title");
+		System.out.println(tropeName);
+		
+		// Get trope content
 		String tropeContent = (String) jsonContent.get("content");	
 
-		
-		//Get trope url
+		// Get trope url
 		String tropeUrl = (String) jsonContent.get("url");	
 
-		//Get list of media links and convert it to a list of media
+		// Get laconic content
+		//String laconic = (String) jsonContent.get("laconic");
+		String laconic = "";
+		
+		// Get related tropes
+		JSONArray nonMediaLinks = (JSONArray) jsonContent.get("non_media_links");
+		List<String> relatedTropes = parseJsonRelatedTropes(nonMediaLinks);
+		
+		// Get list of media links and convert it to a list of media
 		JSONArray mediaLinks = (JSONArray) jsonContent.get("media_links");
 		Map<String, List<String>> tropeMedia = parseJsonMedia(mediaLinks);
 		
-		Trope t = new Trope(tropeName, tropeContent, tropeUrl, tropeMedia);
-		
-		return t;
+		return new Trope(tropeName, tropeContent, tropeUrl, laconic, relatedTropes, tropeMedia);
 	}
     
-	private static List<JSONObject> getJsonData(final File folder) {
+	private static Pair<List<JSONObject>, List<JSONObject>> getJsonData(final File folder) {
 
-		List<JSONObject> tropeData = new ArrayList<JSONObject>();
+		Pair<List<JSONObject>, List<JSONObject>> allData = new Pair<List<JSONObject>, List<JSONObject>>(new ArrayList<JSONObject>(), new ArrayList<JSONObject>());
 		
     	//JSON parser object to parse read file
 		JSONParser jsonParser = new JSONParser();
 		
 	    for (final File fileEntry : folder.listFiles()) {
 	        if (fileEntry.isDirectory()) {
-	            tropeData.addAll(getJsonData(fileEntry));
+	        	Pair<List<JSONObject>, List<JSONObject>> data = getJsonData(fileEntry);
+	        	allData.trope.addAll(data.trope);
+	        	allData.laconic.addAll(data.laconic);
 	        } else {
-	        	if(!fileEntry.getName().contains("laconic")) { // TODO
-		        	try (FileReader reader = new FileReader(fileEntry)) // TODO comprobar consumo de memoria
-		    		{
-		    			//Read JSON file
-		                Object obj = jsonParser.parse(reader);
-	
-		                JSONObject trope = (JSONObject) obj;
-		                
-		                tropeData.add(trope);
-		                
-		    		} catch (FileNotFoundException e) {
-		                e.printStackTrace();
-		                System.out.println("File " + fileEntry.getName());
-		            } catch (IOException e) {
-		                e.printStackTrace();
-		                System.out.println("File " + fileEntry.getName());
-		            } catch (ParseException e) {
-		                e.printStackTrace();
-		                System.out.println("File " + fileEntry.getName());
-		            }
-	        	}
+	        	try (FileReader reader = new FileReader(fileEntry)) // TODO comprobar consumo de memoria
+	    		{
+	    			//Read JSON file
+	                Object obj = jsonParser.parse(reader);
+	                
+	                JSONObject trope = (JSONObject) obj;
+	                
+	                if(fileEntry.getName().contains("laconic")) {
+	                	allData.laconic.add(trope);
+	                } else {
+		                allData.trope.add(trope);
+	                }
+	                
+	    		} catch (FileNotFoundException e) {
+	                e.printStackTrace();
+	                System.out.println("File " + fileEntry.getName());
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	                System.out.println("File " + fileEntry.getName());
+	            } catch (ParseException e) {
+	                e.printStackTrace();
+	                System.out.println("File " + fileEntry.getName());
+	            }
 	        }
 	    }
 	    
-	    return tropeData;
+	    return allData;
 	    
 	}
 	
@@ -226,13 +343,31 @@ public class TropeIndexer {
 
     	String dataFolder = "C:\\Users\\Diego\\Documents\\Universidad\\MUEI\\Recuperacion de la informacion y Web Semantica\\Practica RI\\riws-crawler\\crawler\\data";
     	final File folder = new File(dataFolder);
-    	List<JSONObject> tropeData = getJsonData(folder);
+    	Pair<List<JSONObject>, List<JSONObject>> allData = getJsonData(folder);
+    	List<JSONObject> tropeData = allData.trope;
+    	List<JSONObject> laconicData = allData.laconic;
     	
+    	List<Trope> tropes = new ArrayList<Trope>();
+    	
+    	for(JSONObject data : tropeData) {
+    		Trope trope = parseJsonTrope(data);
+    		tropes.add(trope);
+    	}
+    	
+    	for(JSONObject data : laconicData) {
+    		Trope laconic = parseJsonLaconic(data);
+    		for(Trope t : tropes) {
+    			if(t.Name == laconic.Name) {
+    				tropes.get(tropes.indexOf(t)).Laconic = laconic.Content;
+    				break;
+    			}
+    		}
+    	}
+
     	// TODO eliminar variable debug
     	int i = 0;
-    	int imax = 2;
-    	for(JSONObject data : tropeData) {
-    		Trope t = parseJsonTrope(data);
+    	int imax = 20;
+    	for(Trope t : tropes) {
     		try {
     			indexTropeDocument(t);
     		} catch (IOException e) {
